5d9f801f5e9c95e17bc390a45ca71e60c6ae97be
Alp Senay <alps2789@nycstudents.net>
2023-10-08 23:30:22 -0400

    Working

--- scripting.js
+++ scripting.js
@@ -1,3 +1,29 @@
 function generateRandom() {
     return 1 + Math.floor(Math.random()*100);
 }
+
+
+const para = document.createElement('p');
+
+
+function showRandomForTesting() {
+    let n = generateRandom();
+    para.textContent = `${n}`;
+    document.body.appendChild(para);
+}
+
+const add1Button = document.querySelector('button');
+
+var formValue = document.querySelector('input[name="formValue"]');
+
+function add1() {
+    formValue.value = formValue.value + 1;
+}
+
+add1Button.addEventListener('click', add1);
+
+
+showRandomForTesting();
+para.textContent = "Testing: " + para.textContent;
+
+
